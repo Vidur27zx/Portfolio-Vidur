1e5bf4cca22d6d7a83ce4f372a86880cc7db7f9b
Vidur Ramachandran <vidur2002@gmail.com>
2026-04-05 01:04:32 +0530

    Apply ghostly mobile canvas aesthetic and reduce game shake farther

--- scripts/game/collisions.js
+++ scripts/game/collisions.js
@@ -65,7 +65,7 @@ export function handleBulletCollisions(state, updateHud) {
 
       state.asteroids.splice(i, 1);
       bullet.life = 0;
-      state.shake = 2;
+      state.shake = 1.4;
       updateHud();
       break;
     }
